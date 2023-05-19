4274a1f6a0ae0b0d426e0713db93ddb98f2c7fac
Luiz Filipe <luizfilipemr17@gmail.com>
2023-05-19 14:17:21 -0300

    Pacient implementation with the db

--- src/main/java/com/lv/hospital/entities/Pacient.java
+++ src/main/java/com/lv/hospital/entities/Pacient.java
@@ -2,11 +2,22 @@ package com.lv.hospital.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "pacient")
 public class Pacient implements Serializable{
     
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private Integer age;
 
