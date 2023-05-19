9d977efcb02c8eda880dee9cf8a55d2b3c648e00
Luiz Filipe <luizfilipemr17@gmail.com>
2023-05-19 14:15:01 -0300

    module-info update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.lv.hospital {
     requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.persistence;
+    requires java.sql;
 
     opens com.lv.hospital to javafx.fxml;
     opens com.lv.hospital.controllers to javafx.fxml;
