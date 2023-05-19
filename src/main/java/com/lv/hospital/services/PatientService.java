b3cc61021398227da6a10f2fa86d94347aabcb85
Luiz Filipe <luizfilipemr17@gmail.com>
2023-05-19 18:26:19 -0300

    PatientService

---
+++ src/main/java/com/lv/hospital/services/PatientService.java
@@ -0,0 +1,53 @@
+package com.lv.hospital.services;
+
+import java.util.List;
+
+import com.lv.hospital.entities.Patient;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class PatientService {
+    
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    public PatientService() {
+        emf = Persistence.createEntityManagerFactory("hospital");
+        em = emf.createEntityManager();
+    }
+    
+    public void save(Patient obj) {
+        em.getTransaction().begin();
+        em.persist(obj);
+        em.getTransaction().commit();
+    }
+
+    public List<Patient> findAll() {
+        return em.createQuery("SELECT p FROM Patient p", Patient.class).getResultList();
+    }
+
+    public Patient findById(Long id) {
+        return em.find(Patient.class, id);
+    }
+
+    public void deleteById(Long id) {
+        em.getTransaction().begin();
+        Patient obj = findById(id);
+        em.remove(obj);
+        em.getTransaction().commit();
+    }
+    
+    public void update(Patient obj) {
+        em.getTransaction().begin();
+        em.merge(obj);
+        em.getTransaction().commit();
+    }
+
+
+    public void close() {
+        em.close();
+        emf.close();
+    }
+}
