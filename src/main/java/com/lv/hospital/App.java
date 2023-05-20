43321f11f026ce4b2266f427cae1f6dd2ac4a894
Luiz Filipe <luizfilipemr17@gmail.com>
2023-05-20 11:46:36 -0300

    PatientService Tests

--- src/main/java/com/lv/hospital/App.java
+++ src/main/java/com/lv/hospital/App.java
@@ -8,10 +8,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import com.lv.hospital.entities.GlasgowComaScale;
-import com.lv.hospital.entities.Patient;
-import com.lv.hospital.services.PatientService;
-
 
 /**
  * JavaFX App
@@ -37,36 +33,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-
-
-        PatientService pService = new PatientService();
-        // Testing PatientService
-
-        Patient p1 = new Patient(null, "João", 20);
-
-        pService.save(p1);
-
-        // Testing GlasgowComaScaleService
-
-        GlasgowComaScale g1 = new GlasgowComaScale(null, 4, 5, 6, 2, p1);
-
-        pService.updateGlasgowComaScaleByUserId(p1.getId(), g1);
-
-        System.out.println(pService.findById(p1.getId()).getGlasgowComaScale().getTotal());
-        System.out.println(pService.findById(p1.getId()).getGlasgowComaScale().getResult());
-
-        g1.setEyeOpening(6);
-        g1.setVerbalResponse(-5);
-
-        pService.updateGlasgowComaScaleByUserId(p1.getId(), g1);
-
-        System.out.println(pService.findById(p1.getId()).getGlasgowComaScale().getTotal());
-        System.out.println(pService.findById(p1.getId()).getGlasgowComaScale().getResult());
-
-        System.out.println("OK!");
-
-        pService.close();
-        // launch(args);
+        launch(args);
     }
 
 }
---
+++ src/test/java/com/lv/hospital/services/PatientServiceTest.java
@@ -0,0 +1,108 @@
+package com.lv.hospital.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import com.lv.hospital.entities.GlasgowComaScale;
+import com.lv.hospital.entities.Patient;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class PatientServiceTest {
+
+    private static PatientService patientService;
+    private static Long savedPatientId;
+
+    @BeforeAll
+    public static void setup() {
+        patientService = new PatientService();
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        patientService.close();
+    }
+
+    @Test
+    @Order(1)
+    public void testSave() {
+        Patient patient = new Patient(null, "Test", 20, new GlasgowComaScale(null, 2, 3, 2, 0, null));
+        patientService.save(patient);
+        savedPatientId = patient.getId();
+
+        assertNotNull(savedPatientId);
+    }
+
+    @Test
+    @Order(2)
+    public void testFindAll() {
+        List<Patient> patients = patientService.findAll();
+
+        assertNotNull(patients);
+        assertFalse(patients.isEmpty());
+    }
+
+    @Test
+    @Order(3)
+    public void testFindById() {
+        Patient patient = patientService.findById(savedPatientId);
+
+        assertNotNull(patient);
+        assertEquals(savedPatientId, patient.getId());
+    }
+
+    @Test
+    @Order(4)
+    public void testUpdate() {
+        Patient patient = patientService.findById(savedPatientId);
+
+        Patient originalPatient = new Patient(patient.getId(), patient.getName(), patient.getAge());
+        patient.setName(patient.getName() + " Updated");
+        patient.setAge(patient.getAge() + 1);
+        patientService.update(patient);
+
+        Patient updatedPatient = patientService.findById(savedPatientId);
+
+        assertNotNull(updatedPatient);
+        assertNotEquals(updatedPatient.getName(), originalPatient.getName());
+        assertNotEquals(updatedPatient.getAge(), originalPatient.getAge());
+    }
+
+    @Test
+    @Order(5)
+    public void testUpdateGlasgowComaScaleByUserId() {
+        Patient patient = patientService.findById(savedPatientId);
+        GlasgowComaScale gcsBeforeUpdate = patient.getGlasgowComaScale();
+
+        GlasgowComaScale gcsToUpdate = new GlasgowComaScale(null, 4, 5, 6, 2, patient);
+
+        patientService.updateGlasgowComaScaleByUserId(savedPatientId, gcsToUpdate);
+
+        GlasgowComaScale gcsAfterUpdate = patientService.findById(savedPatientId).getGlasgowComaScale();
+
+        assertNotNull(gcsAfterUpdate);
+        assertNotEquals(gcsBeforeUpdate.getTotal(), gcsAfterUpdate.getTotal());
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteById() {
+        patientService.deleteById(savedPatientId);
+
+        Patient patient = patientService.findById(savedPatientId);
+
+        assertNull(patient);
+    }
+
+}
