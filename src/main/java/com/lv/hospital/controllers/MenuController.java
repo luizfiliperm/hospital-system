d6f1768a7186eb321c3c11b3b0dde4af610ecda2
Vitoria <mvitoriaberto2@gmail.com>
2023-05-30 17:29:48 -0300

    Iniciada a estilização da tela de menu

--- src/main/java/com/lv/hospital/controllers/MenuController.java
+++ src/main/java/com/lv/hospital/controllers/MenuController.java
@@ -1,22 +1,11 @@
 package com.lv.hospital.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import com.lv.hospital.App;
-
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class MenuController implements Initializable{
+public class MenuController {
 
     @FXML
     private Label lbDoctorName;
-
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-       lbDoctorName.setText("Bem Vindo, " + App.loggedDoctor.getName());
-    }
 
 }
